111d7d82d599779e5d2cefcd0ecbcce9be7317a3
alejandrardecajal <alejandrardecajal@gmail.com>
2022-04-28 11:18:40 -0300

    Agregado de logos en HEADER

--- PIAle/src/app/app.module.ts
+++ PIAle/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AcercaDeComponent,
+    LogoAPComponent,
+    SocialComponent,
     
   ],
   imports: [
