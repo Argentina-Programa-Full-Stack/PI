56815cbe48bff6656c4f771799aa3ff874d15be8
alejandrardecajal <alejandrardecajal@gmail.com>
2022-07-27 20:01:24 -0300

    Proyectos funcionando
    A partir de Experiencia v16

--- PIAle/porfolio-frontend/src/app/app.module.ts
+++ PIAle/porfolio-frontend/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewEducacionComponent } from './components/educacion/new-educacion.component';
 import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
+import { NewProyectoComponent } from './components/proyectos/new-proyecto.component';
+import { EditProyectosComponent } from './components/proyectos/edit-proyectos.component';
 
 @NgModule({
   declarations: [
@@ -41,6 +43,8 @@ import { EditEducacionComponent } from './components/educacion/edit-educacion.co
     EditExperienciaComponent,
     NewEducacionComponent,
     EditEducacionComponent,
+    NewProyectoComponent,
+    EditProyectosComponent,
     
   ],
   imports: [
